9065fb89f0bb07d7e698b9f6578796d825396591
PetryDB <112121566+PetryDB@users.noreply.github.com>
2022-10-18 19:22:47 -0300

    Aula 22
    Aula 22

--- server/config/express.js
+++ server/config/express.js
@@ -7,8 +7,8 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 app.set('clientPath', path.join(__dirname, '../..', 'client'));
- console.log(app.get('clientPath'));
- app.use(express.static(app.get('clientPath')));
+console.log(app.get('clientPath'));
+app.use(express.static(app.get('clientPath')));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
